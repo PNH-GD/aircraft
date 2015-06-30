245626758f23f21e58fd7558647f4ef6f8cf98fa
HDPLocust <tarrantallegra@gmail.com>
2015-07-01 01:32:10 +0300

    +

--- lib/req.lua
+++ lib/req.lua
@@ -4,11 +4,10 @@ kb = love.keyboard
 ms = love.mouse
 pi = math.floor(math.pi*1000)/1000
 -------------------------requires-------------------------
-require "lib/class"
-require "lib/binary"
+--require "lib/class"
+--require "lib/binary"
 require "lib/func"
 require "lib/camera"
-require "lib/controls"
 require "lib/base"
 require "lib/engine"
 camera = require "lib/CameraAPI"

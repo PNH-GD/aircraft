7f74ef0f74fe1ebf530ebaf2c01174cc151e7361
HDPLocust <tarrantallegra@gmail.com>
2015-07-01 01:07:50 +0300

    Create req.lua

---
+++ lib/req.lua
@@ -0,0 +1,17 @@
+--------------------------defines-------------------------
+gr = love.graphics
+kb = love.keyboard
+ms = love.mouse
+pi = math.floor(math.pi*1000)/1000
+-------------------------requires-------------------------
+require "lib/class"
+require "lib/binary"
+require "lib/func"
+require "lib/camera"
+require "lib/controls"
+require "lib/base"
+require "lib/engine"
+camera = require "lib/CameraAPI"
+require "lib/camera"
+require "lib/menu"
+
